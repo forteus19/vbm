7b092b431e0697bc677c10a36084fe41c05c3e66
forteus19 <jaxonpowe@gmail.com>
2025-09-05 18:06:34 -0400

    Update to 0.7.0.20b

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("de.undercouch.download") version "5.6.0"
 }
 
-val bfVersion = "0.7.0.19b"
+val bfVersion = "0.7.0.20b"
 
 group = "red.vuis.vbm"
 version = "${bfVersion}-SNAPSHOT"
@@ -18,7 +18,7 @@ repositories {
     mavenCentral()
 }
 
-val bfDownloadUrl = "https://cdn.modrinth.com/data/hTexWmdS/versions/o9OVWGXx/BlockFront-1.21.1-0.7.0.19b-RELEASE.jar"
+val bfDownloadUrl = "https://cdn.modrinth.com/data/hTexWmdS/versions/ch0WenkJ/BlockFront-1.21.1-0.7.0.20b-RELEASE.jar"
 val intermediaryDownloadUrl = "https://raw.githubusercontent.com/forteus19/bf-intermediary/main/intermediary/${bfVersion}.tiny"
 
 val vbmBuildFile = layout.buildDirectory.file("vbm").get().asFile
