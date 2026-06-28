57fc5d7255453048039ef846774a2af3eb4d0ad3
forteus19 <jaxonpowe@gmail.com>
2026-06-27 21:35:54 -0400

    Update to 0.9.0.6b

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("de.undercouch.download") version "5.6.0"
 }
 
-val bfVersion = "0.9.0.5b"
+val bfVersion = "0.9.0.6b"
 
 group = "red.vuis.vbm"
 version = "${bfVersion}-SNAPSHOT"
@@ -17,7 +17,7 @@ repositories {
     mavenCentral()
 }
 
-val bfDownloadUrl = "https://cdn.modrinth.com/data/hTexWmdS/versions/46qXqpd9/BlockFront-1.21.1-0.9.0.5b-RELEASE.jar"
+val bfDownloadUrl = "https://cdn.modrinth.com/data/hTexWmdS/versions/DBdpdDiA/BlockFront-1.21.1-0.9.0.6b-RELEASE.jar"
 val intermediaryDownloadUrl = "https://raw.githubusercontent.com/forteus19/bf-intermediary/main/intermediary/${bfVersion}.tiny"
 
 val vbmBuildFile = layout.buildDirectory.file("vbm").get().asFile
